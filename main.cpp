e28c5acb9f568e850c114cc7787d8e444e68c594
Alex7498-SV <00068817@uca.edu.sv>
2019-10-07 09:55:56 -0600

    inicio de la funcion

--- main.cpp
+++ main.cpp
@@ -2,23 +2,30 @@
 
 using namespace std;
 
-int lineas[2][5]{
-    {3,4,5,1,6},
-    {1,4,8,2,3}
+int lineas[2][3]{
+    {8,5,10},
+    {4,2,3}
 };
 
-int diagonales[2][4]{
-    {2,4,7,2},
-    {1,4,6,9}
+int diagonales[2][2]{
+    {2,6},
+    {1,1}
 };
 
-int tInicial[2]{2,4};
+int tInicial[2]{7,2};
 
-int tFinal[2]{7,6};
+int tFinal[2]{5,2};
 
-//void fastestWay(int a[2][5], int t[2][5], ){}
+void fastestWay(int lineas[2][3], int diagonal[2][2], int iniciales[2], int finales[2], int longitud){
+    int fWay[longitud];
+    int sWay[longitud];
+    fWay[0] = iniciales[0] + lineas[0][0];
+    sWay[0] = iniciales[1] + lineas[1][0];
+    cout<<fWay[0];
+    cout<<sWay[0];
+}
 
 int main()
 {
-
+   fastestWay(lineas, diagonales, tInicial, tFinal, 3);
 }
