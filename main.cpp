fc1c6df5fa8640f2aa984573fe0889cc5d2da74e
Alex7498-SV <00068817@uca.edu.sv>
2019-10-07 10:35:44 -0600

    Todavia no funca

--- main.cpp
+++ main.cpp
@@ -16,16 +16,40 @@ int tInicial[2]{7,2};
 
 int tFinal[2]{5,2};
 
-void fastestWay(int lineas[2][3], int diagonal[2][2], int iniciales[2], int finales[2], int longitud){
-    int fWay[longitud];
-    int sWay[longitud];
-    fWay[0] = iniciales[0] + lineas[0][0];
-    sWay[0] = iniciales[1] + lineas[1][0];
-    cout<<fWay[0];
-    cout<<sWay[0];
+void fastestWay(int a[2][3], int t[2][2], int e[2], int x[2], int longitud){
+    int f1[longitud];
+    int f2[longitud];
+    int fin;
+    f1[0] = e[0] + a[0][0];
+    f2[0] = e[1] + a[1][0];
+
+    for(int j = 1; j<= longitud; j++){
+        if(f1[j-1]+a[1][j] <= f2[j-1]+t[2][j-1]+ a[1][j]){
+            f1[j] = f1[j-1] + a[1][j];
+        }
+        else{
+            f1[j] = f2[j-1]+t[2][j-1]+ a[1][j];
+        }
+        if (f2[j-1] + a[2][j] <= f1[j-1] + t[1][j-1] + a[2][j]){
+            f2[j] = f2[j-1] + a[2][j];
+        }
+        else{
+            f2[j] = f1[j-1] + t[1][j-1] + a[2][j];
+        }
+    }
+
+    if(f1[longitud] + x[0] <= f2[longitud] + x[1]){
+        fin = f1[longitud] + x[0];
+    }
+    else{
+        fin = f2[longitud] + x[1];
+    }
+
+    cout<<fin;
 }
 
 int main()
 {
    fastestWay(lineas, diagonales, tInicial, tFinal, 3);
+
 }
