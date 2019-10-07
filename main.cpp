4ce48a04991875ec744817ab936e3c86c1df7361
Alex7498-SV <00068817@uca.edu.sv>
2019-10-07 09:43:03 -0600

    Inicio del repositorio

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int lineas[2][5]{
+    {3,4,5,1,6},
+    {1,4,8,2,3}
+};
+
+int diagonales[2][4]{
+    {2,4,7,2},
+    {1,4,6,9}
+};
+
+int tInicial[2]{2,4};
+
+int tFinal[2]{7,6};
+
+//void fastestWay(int a[2][5], int t[2][5], ){}
+
+int main()
+{
+
+}
